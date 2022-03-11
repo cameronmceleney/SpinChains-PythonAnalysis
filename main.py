179a72b95546521de5a4fadc8b7bb6a0b3ec58bd
Cameron McEleney <c.mceleney.1@research.gla.ac.uk>
2022-03-11 21:07:04 +0000

    edit

--- main.py
+++ main.py
@@ -99,14 +99,20 @@ def temporal_plot(time_data, y_axis_data):
     """
     Plots the given data showing the magnitude against time. Will output the 'signal'.
     """
-    dicts = {0: {"title": "Time Domain Data1", "ylabel": "Test"}}
-    dicts[1] = {"title": "Time Domain Data2", "xlabel": "Cake", "xlim": (0, 5)}
+    dicts = {0: {"title": "Time Domain Data1", "ylabel": "Test"},
+             1: {"title": "Time Domain Data2", "xlabel": "Cake", "xlim": (0, 5)},
+             2: {"title": "FFT", "xlabel": "Frequency [GHz]", "ylabel": "Amplitude [arb.]", "xlim": (0, 5), "yscale": 'log'},
+             3: {"title": "FFT", "xlabel": "Frequency [GHz]", "xlim": (0, 40), "yscale": 'log'}}
 
-    fig, axes = plt.subplots(2, figsize=(8, 8))
+    fig, axes = plt.subplots(2, 2, figsize=(12, 12), sharey='row')
 
-    for i, ax in enumerate(axes):
-        custom_temporal_plot(time_data / 1e-9, y_axis_data, ax=ax, plt_kwargs=dicts[i])
+    for i, ax in enumerate(axes.flatten()):
+        if i <= 1:
+            custom_temporal_plot(time_data / 1e-9, y_axis_data, ax=ax, plt_kwargs=dicts[i])
+        else:
+            custom_fft_plot(y_axis_data, ax=ax, plt_kwargs=dicts[i])
 
+    plt.suptitle("Data from Spin Site #1")
     plt.tight_layout()
     plt.show()
 
@@ -116,6 +122,40 @@ def custom_temporal_plot(x, y, ax=None, plt_kwargs={}):
         ax = plt.gca()
     ax.plot(x, y)
     ax.set(**plt_kwargs)
+    return ax
+
+def custom_fft_plot(y, ax=None, plt_kwargs={}):
+
+    stepsize = 2.857e-13
+    total_iterations = 141 * 1e3
+    gamma = 29.2  # 28.3
+    H_static = 0.1
+    freq_drive = 30.0  # In GHz
+    # total_datapoints = 1e7  # Can be used as the 'number of samples'
+    hz_to_ghz = 1e-9  # Multiply by this to convert Hz to GHz
+
+    # Set values using simulation parameters
+    timeInterval = stepsize * total_iterations
+    nSamples = len(y)
+    # Could also find this by multiplying the stepsize by the number of iterations between data recordings
+    dt = timeInterval / nSamples
+
+    # Compute the FFT
+    fourierTransform = np.fft.fft(y)  # Normalize amplitude
+    fourierTransform = fourierTransform[range(int(nSamples / 2))]  # Exclude sampling frequency
+    frequencies = (np.arange(int(nSamples / 2)) / (dt * nSamples)) * hz_to_ghz
+
+    if ax is None:
+        ax = plt.gca()
+    ax.plot(frequencies, abs(fourierTransform), marker='o', lw=1, color='red', markerfacecolor='black', markeredgecolor='black')
+    ax.set(**plt_kwargs)
+    ax.axvline(x=gamma * H_static, label=f"Natural. {gamma * H_static:2.2f}")
+    ax.axvline(x=freq_drive, label=f"Driving. {freq_drive}", color='green')
+    ax.legend(loc=1, bbox_to_anchor=(0.975, 0.975),
+              frameon=True, fancybox=True, facecolor='white', edgecolor='white',
+              title='Freq. List [GHz]', fontsize=12)
+
+    ax.grid(color='white')
     return ax
 
 
