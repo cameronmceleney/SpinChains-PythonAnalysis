93b808a7eb37db1288634d9cabb69df81e7d1f10
Cameron McEleney <c.mceleney.1@research.gla.ac.uk>
2022-03-11 16:12:28 +0000

    Add functions to detect Operating System. Add functions to generate a tree of files

--- globalvariables.py
+++ globalvariables.py
@@ -3,6 +3,9 @@
 
 # Standard libraries
 from sys import platform, exit
+from datetime import datetime
+import os as os
+import errno as errno
 
 # 3rd Party Packages
 # Add here
@@ -22,33 +25,103 @@ from sys import platform, exit
     Filename    : globalvariables
     IDE         : PyCharm
 """
-current_date_mac = "10 Mar 22"
-current_date_windows = "03 Mar 22"
 
 
-def set_file_paths():
-    # Detect the user's operating system in order to set file_paths
+def generate_OS_dir_tree(set_custom_name=False):
+    """Detect the user's operating system in order to set file_paths"""
+
+    if set_custom_name:
+        parent_name = custom_name()
+    else:
+        parent_name = todays_date()
 
     if platform == "linux" or platform == "linux2":
         # linux
-        print("Detected Linux. Exiting")
+        print("Detected Linux. This OS is not yet supported. Exiting")
 
         exit()
 
     elif platform == "darwin":
         # OS X
-        mac_dir_root = "/Users/cameronmceleney/CLionProjects/Data/"
+        mac_dir_root = "/Users/cameronmceleney/CLionProjects/Data/"  # Location of my C++ data on Mac
 
-        input_data_directory = f"{mac_dir_root}{current_date_mac}/RK2 Shockwaves Tests Data/"
-        output_data_directory = f"{mac_dir_root}{current_date_mac}/RK2 Shockwaves Tests Outputs/"
+        create_directory(mac_dir_root, parent_name)
+
+        input_data_directory = f"{mac_dir_root}{parent_name}/RK2 Shockwaves Tests Data/"
+        output_data_directory = f"{mac_dir_root}{parent_name}/RK2 Shockwaves Tests Outputs/"
 
         return input_data_directory, output_data_directory
 
     elif platform == "win32":
         # Windows
-        windows_dir_root = "D:\\Data\\"
+        windows_dir_root = "D:\\Data\\"  # Location of my C++ data on Windows
 
-        input_data_directory = f"{windows_dir_root}{current_date_windows}\\RK2 Shockwaves Tests Data\\"
-        output_data_directory = f"{windows_dir_root}{current_date_windows}\\RK2 Shockwaves Tests Outputs\\"
+        create_directory(windows_dir_root, parent_name)
+
+        input_data_directory = f"{windows_dir_root}{parent_name}\\RK2 Shockwaves Tests Data\\"
+        output_data_directory = f"{windows_dir_root}{parent_name}\\RK2 Shockwaves Tests Outputs\\"
 
         return input_data_directory, output_data_directory
+
+
+def custom_name():
+    """Need to add description"""
+    users_custom_name = str(input("Enter the name of the parent directory: "))
+    return users_custom_name
+
+
+def todays_date():
+    """
+    Finds and returns today's date in DD MMM YY format.
+
+    For details on how to change the date format, see the official `documentation <https://strftime.org/>`_.
+
+    :return: Today's date.
+    """
+
+    date = datetime.today().strftime("%d %b %y")
+
+    return date
+
+
+def create_directory(root_dir_path, parent_dir_name, show_errors=False):
+    """
+    Create a tree of subdirectories to save simulation data.
+
+    The following notes should be observed:
+
+    * The Python code should be run **before** the C++ simulations to ensure all needed directories exist.
+
+    :param str root_dir_path: Absolute path to location where parent will be created.
+    :param str parent_dir_name: Name of parent directory.
+    :param bool show_errors: Set (True) to show when a directory couldn't be created.
+
+    :return: Nothing.
+    """
+
+    # Default behaviour is to use today's date as the dir name; matching the behaviour of C++ code
+    parent_dir_path = os.path.join(root_dir_path, parent_dir_name)
+
+    # Create set (to avoid duplicates) of all the directories to be made under the parent_dir
+    sub_directories = {"RK2 Shockwaves Tests Data", "RK2 Shockwaves Tests Outputs"}
+    path_list = []
+
+    for i, val in enumerate(sub_directories):
+        # Create list of paths to all subdirectories to be created. Done separately as future functions are to be added
+        path_list.append(os.path.join(parent_dir_path, val))
+
+    for i, val in enumerate(sub_directories):
+        try:
+            # Try to create each subdirectory (and parent if needed). Always show instances of dirs being created
+            os.makedirs(path_list[i], exist_ok=False)
+            print(f"Directory '{val}' created successfully")
+
+        except OSError as exc:
+            if exc.errno != errno.EEXIST:
+                raise
+
+            # Handles exceptions from raise
+            if show_errors:
+                # User-selected flag in function argument
+                print(f"Directory '{val}' already exists.")
+                pass
