8c44220e082a5e7f9be209007e5d2dd94507b34f
Cameron McEleney <c.mceleney.1@research.gla.ac.uk>
2022-04-04 12:03:40 +0100

    change how GIFs are plotted to show damping and driving regions as shaded boxes

--- main.py
+++ main.py
@@ -33,9 +33,9 @@ def main():
     #                  breaking_paper=False)
 
     system_setup = sp.SystemSetup()
-    system_setup.detect_os(has_custom_name=True)
+    system_setup.detect_os(has_custom_name=False)
 
-    filename_base = "2313" # str(input("Enter the unique identifier of the file: "))
+    filename_base = str(input("Enter the unique identifier of the file: "))
     Dataset1 = das.PlotImportedData(filename_base, system_setup.input_dir(), system_setup.output_dir(),
                                     file_component='mx')
     Dataset1.call_methods()
