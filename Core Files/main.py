4fbfbab79ca5e3ffb2c6b86e771bea100ce9f679
Cameron McEleney <c.mceleney.1@research.gla.ac.uk>
2022-12-05 14:36:47 +0000

    Allow for ascent/descent of text-menu in data_analysis.py through the keyword 'back'

--- Core Files/main.py
+++ Core Files/main.py
@@ -36,7 +36,7 @@ def main():
     _should_use_eigens = False
 
     system_setup = sp.SystemSetup()
-    system_setup.detect_os(False)
+    system_setup.detect_os(True, "2022-12-01")
 
     filename_base = str(input("Enter the unique identifier of the file: "))
     if not _should_use_eigens:
