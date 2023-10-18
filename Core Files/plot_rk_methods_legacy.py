bb3857c7e13f7afa36f1677571740ebfde671a6d
Cameron McEleney <c.mceleney.1@research.gla.ac.uk>
2023-10-18 18:35:33 +0100

    Finished merging time_variation2 and time_variation3 in plots_for_rk_methods.py.

--- Core Files/plot_rk_methods_legacy.py
+++ Core Files/plot_rk_methods_legacy.py
@@ -830,3 +830,271 @@ def create_time_variation(self, spin_site, colour_precursors=False, annotate_pre
             plt.show()
         else:
             self.fig.savefig(f"{self.output_filepath}_site{spin_site}_tv0.png", bbox_inches="tight")
+
+
+def create_time_variation3(self, interactive_plot=False, use_inset_1=True, use_lower_plot=False):
+    """
+    LEGACY METHOD FROM PAPERFIGURES. WILL NOT RUN IN THIS MODULE. Plot the Heaviside Fn and Disp Reln.
+
+    One should ensure that the site being plotted is not inside either of the driving- or damping-regions.
+
+    :return: Saves a .png image to the designated output folder.
+    """
+
+    if self._fig is None:
+        self._fig = plt.figure(figsize=(4.5, 3.375))
+
+    num_rows = 2
+    num_cols = 3
+    if use_lower_plot:
+        ax1 = plt.subplot2grid((num_rows, num_cols), (0, 0), rowspan=int(num_rows / 2),
+                               colspan=num_cols, fig=self._fig)
+    else:
+        ax1 = plt.subplot2grid((num_rows, num_cols), (0, 0), rowspan=int(num_rows),
+                               colspan=num_cols, fig=self._fig)
+
+    SAMPLE_RATE = int(5e2)  # Number of samples per nanosecond
+
+    DURATION = int(2)  # Nanoseconds
+    FREQUENCY = int(8)  # GHz
+
+    # Number of samples in normalized_tone
+
+    # def generate_sine_wave(freq, sample_rate, duration, delay_start=0, only_delay=False):
+    #     delay = int(sample_rate * delay_start)
+    #     t = np.linspace(0, duration, sample_rate * duration, endpoint=False)
+    #     y_1 = np.zeros(delay)
+    #     y_2 = np.sin((2 * np.pi * freq) * t[delay:])
+    #     y_con = np.concatenate((y_1, y_2))
+    #
+    #     if only_delay:
+    #         start = delay
+    #         sample_rate = int(sample_rate * delay_start)
+    #         end = int(sample_rate * duration)
+    #         return t[start:end], y_con[start:end]
+    #
+    #     return t, y_con
+
+    def generate_sine_wave(freq, sample_rate, duration, delay_start=0.0, only_delay=False):
+
+        if only_delay:
+            num_samples = int(sample_rate * (duration - delay_start))
+            sample_rate = int(num_samples / (duration - delay_start))
+
+            t = np.linspace(delay_start, duration, num_samples, endpoint=False)
+            y = np.sin((2 * np.pi * freq) * t)
+
+            return t, y, sample_rate, num_samples
+
+        else:
+            delay = int(sample_rate * delay_start)
+            num_samples = int(sample_rate * duration)
+
+            t = np.linspace(0, duration, num_samples, endpoint=False)
+            y_1 = np.zeros(delay)
+            y_2 = np.sin((2 * np.pi * freq) * t[delay:])
+            y_con = np.concatenate((y_1, y_2))
+
+            return t, y_con, sample_rate, num_samples
+
+    # Generate a 15 GHz sine wave that lasts for 5 seconds
+    time_instant, signal_instant, sample_rate_instant, num_samples_instant = generate_sine_wave(FREQUENCY,
+                                                                                                SAMPLE_RATE,
+                                                                                                DURATION, 0, False)
+    time_delay, signal_delay, sample_rate_delay, num_samples_delay = generate_sine_wave(FREQUENCY, SAMPLE_RATE,
+                                                                                        DURATION, 1.0, False)
+    time_delay2, signal_delay2, sample_rate_delay2, num_samples_delay2 = generate_sine_wave(FREQUENCY, SAMPLE_RATE,
+                                                                                            DURATION, 1.0, True)
+
+    time_instant_fft, signal_instant_fft = rfftfreq(num_samples_instant, 1 / sample_rate_instant), rfft(
+        signal_instant)
+    time_delay_fft, signal_delay_fft = rfftfreq(num_samples_delay, 1 / sample_rate_delay), rfft(signal_delay)
+    time_delay_fft2, signal_delay_fft2 = rfftfreq(num_samples_delay2, 1 / sample_rate_delay2), rfft(signal_delay2)
+
+    ax1.plot(time_delay_fft, np.abs(signal_delay_fft), marker='', lw=2.0, color='#ffb55a', markerfacecolor='black',
+             markeredgecolor='black', label="1", zorder=1.2)
+    ax1.plot(time_instant_fft, np.abs(signal_instant_fft), marker='', lw=1.5, ls='--', color='#64bb6a',
+             markerfacecolor='black', markeredgecolor='black', label="0", zorder=1.3)
+    ax1.plot(time_delay_fft2, np.abs(signal_delay_fft2), marker='', lw=2.0, ls='-.', color='red',
+             markerfacecolor='black',
+             markeredgecolor='black', label="1", zorder=1.2)
+
+    # ax1.set(xlim=(5.001, 24.999), ylim=(1e-13, 1e4),
+    #        xlabel="Frequency (GHz)", ylabel="Amplitude\n(arb. units)", yscale='log')
+
+    ax1.set(xlim=(0.001, 15.999), ylim=(1e0, 1e4),
+            xlabel="Frequency (GHz)", ylabel="Amplitude\n(arb. units)", yscale='log')
+
+    # ax1.plot(x1, y1, lw=2, color='#0289F7', zorder=1.2)
+    # ax1.plot(x2, y2, lw=2, ls='-', color='#64bb6a', zorder=1.1)
+    # ax1.set(xlim=(0, 2), ylim=(-1, 1),
+    #         xlabel="Time (ns)", ylabel="Amplitude\n(arb. units)")
+    # self._tick_setter(ax1, 1.0, 0.25, 3, 2, is_fft_plot=False, yaxis_num_decimals=1, yscale_type='p')
+
+    ax1.xaxis.labelpad = -2
+    ax1.yaxis.labelpad = -0
+    self._tick_setter(ax1, 5, 1, 4, 4, is_fft_plot=True)
+
+    ########################################
+    if use_inset_1:
+        if use_lower_plot:
+            ax1_inset = inset_axes(ax1, width=1.3, height=0.72, loc="upper right", bbox_to_anchor=[0.995, 1.175],
+                                   bbox_transform=ax1.transAxes)
+        else:
+            ax1_inset = inset_axes(ax1, width=1.3, height=0.72, loc="upper right", bbox_to_anchor=[0.995, 0.98],
+                                   bbox_transform=ax1.transAxes)
+
+        ax1_inset.plot(time_instant, signal_instant, lw=0.5, color='#64bb6a', zorder=1.1)
+        ax1_inset.plot(time_delay, signal_delay, lw=0.5, ls='-.', color='#ffb55a', zorder=1.2)
+        ax1_inset.plot(time_delay2, signal_delay2, lw=0.5, ls='--', color='red', zorder=1.3)
+
+        ax1_inset.set(xlim=[0, 2], ylim=[-1, 1])
+        ax1_inset.set_xlabel('Time (ns)', fontsize=self._fontsizes["tiny"])
+        ax1_inset.yaxis.tick_left()
+        ax1_inset.yaxis.set_label_position("left")
+        ax1_inset.set_ylabel('Amplitude  \n(arb. units)  ', fontsize=self._fontsizes["tiny"], rotation=90,
+                             labelpad=20)
+        ax1_inset.tick_params(axis='both', labelsize=self._fontsizes["mini"])
+
+        ax1_inset.patch.set_color("#f9f2e9")
+        ax1_inset.yaxis.labelpad = 0
+        ax1_inset.xaxis.labelpad = -0.5
+
+        self._tick_setter(ax1_inset, 1.0, 0.25, 3, 2, is_fft_plot=False,
+                          yaxis_num_decimals=0.1, yscale_type='p')
+
+    ########################################
+
+    if use_lower_plot:
+        ax2 = plt.subplot2grid((num_rows, num_cols), (int(num_rows / 2), 0),
+                               rowspan=num_rows, colspan=num_cols, fig=self._fig)
+
+        # ax2.scatter(np.arange(1, len(freqs) + 1, 1), freqs, s=0.5)
+        external_field, exchange_field = 0.1, 132.5
+        gyromag_ratio = 28.8e9 * 2 * np.pi
+        lattice_constant = np.sqrt(5.3e-17 / exchange_field)
+        system_len = 10e-5  # metres
+        max_len = round(system_len / lattice_constant)
+        num_spins_array = np.arange(0, max_len, 1)
+        wave_number_array = (num_spins_array * np.pi) / ((len(num_spins_array) - 1) * lattice_constant)
+        freq_array = gyromag_ratio * (2 * exchange_field * (1 - np.cos(wave_number_array * lattice_constant))
+                                      + external_field)
+
+        hz_2_THz = 1e-12
+        hz_2_GHz = 1e-9
+
+        ax2.plot(wave_number_array * hz_2_GHz, freq_array * hz_2_THz, color='red', ls='-', label=f'Dataset 1')
+        ax2.plot(wave_number_array * hz_2_GHz, gyromag_ratio * (
+                external_field + exchange_field * lattice_constant ** 2 * wave_number_array ** 2) * hz_2_THz,
+                 color='red', alpha=0.4, ls='-', label=f'Dataset 1')
+
+        # These!!
+        # ax2.scatter(np.arccos(1 - ((freqs2 / gamma - h_0) / (2 * h_ex))) / a, freqs2 / 1e12, s=0.5,
+        # c='red', label='paper')
+        # ax2.plot(k, gamma * (2 * h_ex * (1 - np.cos(k * a)) + h_0) / 1e12, color='red', ls='--', label=f'Kittel')
+
+        ax2.set(xlabel="Wavenumber (nm$^{-1}$)", ylabel='Frequency (THz)', xlim=[0, 2], ylim=[0, 5])  # [0, 15.4]
+        self._tick_setter(ax2, 2, 0.5, 3, 2, is_fft_plot=False,
+                          xaxis_num_decimals=1, yaxis_num_decimals=0.0, yscale_type='p')
+
+        # ax2.axhline(y=3.8, xmax=1.0, ls='--', lw=1, color='grey', zorder=0.9) # xmax=0.31
+        # ax2.axhline(y=10.5, xmax=1.0, ls='--', lw=1, color='grey', zorder=0.9)  # xmax=0.68
+        ax2.margins(0)
+        ax2.xaxis.labelpad = -2
+
+        # ax2.text(0.997, -0.13, r"$\mathrm{\dfrac{\pi}{a}}$",
+        #          verticalalignment='center', horizontalalignment='center', transform=ax2.transAxes,
+        #          fontsize=self._fontsizes["smaller"])
+
+        # ax2.text(0.02, 0.88, r"$\mathcal{III}$", verticalalignment='center', horizontalalignment='left',
+        #          transform=ax2.transAxes, fontsize=self._fontsizes["smaller"])
+        # ax2.text(0.02, 0.5, r"$\mathcal{II}$", verticalalignment='center', horizontalalignment='left',
+        #          transform=ax2.transAxes, fontsize=self._fontsizes["smaller"])
+        # ax2.text(0.02, 0.12, r"$\mathcal{I}$", verticalalignment='center', horizontalalignment='left',
+        #          transform=ax2.transAxes, fontsize=self._fontsizes["smaller"])
+        #
+        # ax2.text(0.91, 0.82, f"Decreasing", verticalalignment='center', horizontalalignment='center',
+        #          transform=ax2.transAxes, fontsize=self._fontsizes["tiny"])
+        # ax2.text(0.60, 0.425, f"Linear", verticalalignment='center', horizontalalignment='center',
+        #          transform=ax2.transAxes, fontsize=self._fontsizes["tiny"])
+        # ax2.text(0.41, 0.12, f"Increasing", verticalalignment='center', horizontalalignment='center',
+        #          transform=ax2.transAxes, fontsize=self._fontsizes["tiny"])
+
+        # arrow_ax2_props1 = {"arrowstyle": '-|>', "connectionstyle": "arc3,rad=0.075", "color": "black"}
+        # arrow_ax2_props2 = {"arrowstyle": '-|>', "connectionstyle": "arc3,rad=0.0", "color": "black"}
+        # arrow_ax2_props3 = {"arrowstyle": '-|>', "connectionstyle": "arc3,rad=-0.075", "color": "black"}
+        # ax2.annotate('', xy=(1.665, 2.961), xytext=(1.147, 1.027), va='center', ha='center',
+        #              arrowprops=arrow_ax2_props1, fontsize=self._fontsizes["tiny"], transform=ax2.transAxes)
+        # ax2.annotate('', xy=(3.058, 9.406), xytext=(2.154, 5.098), va='center', ha='center',
+        #              arrowprops=arrow_ax2_props2, fontsize=self._fontsizes["tiny"], transform=ax2.transAxes)
+        # ax2.annotate('', xy=(4.155, 13.213), xytext=(3.553, 11.342), va='center', ha='center',
+        #              arrowprops=arrow_ax2_props3, fontsize=self._fontsizes["tiny"], transform=ax2.transAxes)
+
+        ########################################
+        use_lower_subplot = False
+        if use_lower_subplot:
+            ax2_inset = inset_axes(ax2, width=1.9, height=0.8, loc="lower right", bbox_to_anchor=[0.99, 0.02],
+                                   bbox_transform=ax2.transAxes)
+            D_b = 5.3e-17
+            a1 = lattice_constant
+            a2 = np.sqrt(D_b / 132.5)
+
+            # j_to_meV = 6.24150934190e21
+            num_spins_array1 = np.arange(0, 5000, 1)
+            num_spins_array2 = np.arange(0, 15811, 1)
+            wave_number_array1 = (num_spins_array1 * np.pi) / ((len(num_spins_array1) - 1) * a1)
+            wave_number_array2 = (num_spins_array2 * np.pi) / ((len(num_spins_array2) - 1) * a2)
+
+            ax2_inset.plot(wave_number_array1 * hz_2_GHz,
+                           (D_b * 2 * gyromag_ratio) * wave_number_array1 ** 2 * hz_2_THz, lw=1.5, ls='--',
+                           color='purple',
+                           label='$a=0.2$ nm',
+                           zorder=1.3)
+            ax2_inset.plot(wave_number_array2 * hz_2_GHz,
+                           (D_b * 2 * gyromag_ratio) * wave_number_array2 ** 2 * hz_2_THz, lw=1.5, ls='-',
+                           label='$a=0.63$ nm',
+                           zorder=1.2)
+
+            ax2_inset.set_xlabel('Wavenumber (nm$^{-1}$)', fontsize=self._fontsizes["tiny"])
+            ax2_inset.set_xlim(0, 2)
+            ax2_inset.set_ylim(0, 10)
+            ax2_inset.xaxis.tick_top()
+            ax2_inset.xaxis.set_label_position("top")
+            ax2_inset.yaxis.set_label_position("left")
+            ax2_inset.set_ylabel('Frequency\n(THz)', fontsize=self._fontsizes["tiny"], rotation=90, labelpad=20)
+            ax2_inset.tick_params(axis='both', labelsize=self._fontsizes["tiny"])
+            ax2.margins(0)
+
+            ax2_inset.patch.set_color("#f9f2e9")
+            ax2_inset.yaxis.labelpad = 5
+            ax2_inset.xaxis.labelpad = 2.5
+
+            # self._tick_setter(ax2_inset, 2.5, 0.5, 3, 2, is_fft_plot=False)
+            ax2_inset.ticklabel_format(axis='y', style='plain')
+            ax2_inset.legend(fontsize=self._fontsizes["tiny"], frameon=False)
+
+        ########################################
+
+        # ax1.text(0.025, 0.88, f"(a)", verticalalignment='center', horizontalalignment='left',
+        #          transform=ax1.transAxes, fontsize=self._fontsizes["smaller"])
+
+        # ax2.text(0.975, 0.12, f"(b)",
+        #         verticalalignment='center', horizontalalignment='right', transform=ax2.transAxes,
+        #         fontsize=self._fontsizes["smaller"])
+
+    for ax in [ax1]:
+        ax.tick_params(axis="both", which="both", bottom=True, top=True, left=True, right=True, zorder=1.99)
+        ax.set_axisbelow(False)
+
+    self._fig.subplots_adjust(wspace=1, hspace=0.35)
+
+    if interactive_plot:
+        # For interactive plots
+        def mouse_event(event: Any):
+            print(f'x: {event.xdata} and y: {event.ydata}')
+
+        self._fig.canvas.mpl_connect('button_press_event', mouse_event)
+        self._fig.tight_layout()  # has to be here
+        plt.show()
+    else:
+        self._fig.savefig(f"{self.output_filepath}_dispersion_tv3.png", bbox_inches="tight")
